baff49ff37c1840df44a88f64a8e60025afbb4b9
Sanawar <sanawarsaeed@gmail.com>
2025-09-30 14:29:57 +0000

    docs: add comprehensive documentation for Craftlore Sawtooth blockchain development; include updates on BaseClass and account actions

--- craftlore_tp/models/classes/accounts/buyer.py
+++ craftlore_tp/models/classes/accounts/buyer.py
@@ -6,11 +6,12 @@ Buyer account entity for CraftLore Account TP.
 from pydantic import Field
 
 from . import BaseAccount
-from models.enums import AccountType
+from models.enums import AccountType, BuyerType
 
 class BuyerAccount(BaseAccount):
     """Buyer account that creates work orders or purchases products."""
     account_type: AccountType = Field(default=AccountType.BUYER)
+    buyer_type: BuyerType = Field(default=BuyerType.END_CUSTOMER)
 
     @property
     def forbidden_fields(self) :
